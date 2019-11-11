e14a6bee42ec9c4c31b8cb42c1ddbee506cec4a5
Heather Nuffer <zzzuzz@gmail.com>
2019-11-11 15:45:50 -0700

    Fix: addToSavedList now won't all duplicate movies

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,29 @@ import Movie from './Movies/Movie';
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
+  const isEquivalent = (a, b) => {
+    // To check if two objects are the 'same':
+    let aProps = Object.getOwnPropertyNames(a);
+    let bProps = Object.getOwnPropertyNames(b);
+
+    if (aProps.length !== bProps.length) {
+        return false;
+    }
+
+    for (let i = 0; i < aProps.length; i++) {
+        let propName = aProps[i];
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+    return true;
+}
+
   const addToSavedList = movie => {
+    //Add movie to savedList if movie is not already in the list:
+    if (!savedList.find(item => isEquivalent(item, movie))) {
     setSavedList( [...savedList, movie] );
+    };
   };
 
   return (
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,9 +5,15 @@ const SavedList = props => {
   return (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span key={movie.title} className="saved-movie">{movie.title}</span>
-    ))}
+    {props.list.map(movie => {
+      return (
+        <div key={movie.title}>
+          <Link to={`movies/${movie.id}`}> 
+          <span className="saved-movie">{movie.title}</span>
+          </Link>
+        </div>
+      )
+    })}
     <div className="home-button"><Link to='/'>Home</Link></div>
   </div>
 )};
